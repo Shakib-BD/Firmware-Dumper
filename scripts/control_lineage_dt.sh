f14c7b3cf96ca7f7daf24876ff3080eaa7e59f01
YZBruh <131263805+YZBruh@users.noreply.github.com>
2023-11-11 22:07:19 +0300

    Notes

--- scripts/control_lineage_dt.sh
+++ scripts/control_lineage_dt.sh
@@ -12,26 +12,31 @@ echo "Pushing..."
 g_init=$(git init)
 if [ ! -f "$g_init" ]; then
   echo "Pushing failed!"
+  echo "NOTE: Dump already uploaded."
   exit;
 fi;
 g_branch=$(git branch -M lineage-"${{ env.DCN }}")
 if [ ! -f "$g_branch" ]; then
   echo "Pushing failed!"
+  echo "NOTE: Dump already uploaded."
   exit;
 fi;
 g_add=$(git add .)
 if [ ! -f "$g_add" ]; then
   echo "Pushing failed!"
+  echo "NOTE: Dump already uploaded."
   exit;
 fi;
 g_commit=$(git commit -s -m ""${{ env.DCN }}": LineageOS compatible device tree")
 if [ ! -f "$g_commit" ]; then
   echo "Pushing failed!"
+  echo "NOTE: Dump already uploaded."
   exit;
 fi;
 g_upload=$(gh repo create lineage_device_"${{ env.DB }}"_"${{ env.DCN }}" --public --description="LineageOS Device tree for ${{ env.DCN }}." --source=. --remote=origin --push)
 if [ ! -f "$g_upload" ]; then
   echo "Pushing failed!"
+  echo "NOTE: Dump already uploaded."
   exit;
 fi;
 echo "Succesfull."
