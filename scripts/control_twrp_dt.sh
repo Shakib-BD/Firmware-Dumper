8027e83716a671e7c3a536b31ecf827a85f39a75
YZBruh <131263805+YZBruh@users.noreply.github.com>
2023-11-11 22:08:09 +0300

    Notes

--- scripts/control_twrp_dt.sh
+++ scripts/control_twrp_dt.sh
@@ -12,26 +12,31 @@ echo "Pushing..."
 g_init=$(git init)
 if [ ! -f "$g_init" ]; then
   echo "Pushing failed!"
+  echo "NOTE: Dump and LineageOS tree already uploaded."
   exit;
 fi;
 g_branch=$(git branch -M twrp-"${{ env.DCN }}")
 if [ ! -f "$g_branch" ]; then
   echo "Pushing failed!"
+  echo "NOTE: Dump and LineageOS tree already uploaded."
   exit;
 fi;
 g_add=$(git add .)
 if [ ! -f "$g_add" ]; then
   echo "Pushing failed!"
+  echo "NOTE: Dump and LineageOS tree already uploaded."
   exit;
 fi;
 g_commit=$(git commit -s -m ""${{ env.DCN }}": TWRP compatible device tree")
 if [ ! -f "$g_commit" ]; then
   echo "Pushing failed!"
+  echo "NOTE: Dump and LineageOS tree already uploaded."
   exit;
 fi;
 g_upload=$(gh repo create twrp_device_"${{ env.DB }}"_"${{ env.DCN }}" --public --description="TWRP Device tree for ${{ env.DCN }}." --source=. --remote=origin --push)
 if [ ! -f "$g_upload" ]; then
   echo "Pushing failed!"
+  echo "NOTE: Dump and LineageOS tree already uploaded."
   exit;
 fi;
 echo "Succesfull."
