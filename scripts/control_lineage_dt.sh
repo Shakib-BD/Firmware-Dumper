c5eb7f0b329af9f2658225ce17ccf29bc2d53385
YZBruh <131263805+YZBruh@users.noreply.github.com>
2023-11-12 11:27:48 +0300

    Shows better work

--- scripts/control_lineage_dt.sh
+++ scripts/control_lineage_dt.sh
@@ -9,6 +9,7 @@ if [ ! -f "$check_dt" ]; then
 fi;
 echo "A LineageOS compatible device tree has been created."
 echo "Pushing..."
+sudo rm -rf .git
 g_init=$(git init)
 if [ ! -f "$g_init" ]; then
   echo "Pushing failed!"
@@ -17,6 +18,12 @@ if [ ! -f "$g_init" ]; then
 fi;
 g_branch=$(git branch -M lineage-${{ env.DCN }})
 if [ ! -f "$g_branch" ]; then
+  echo "Pushing failed!"
+  echo "NOTE: Dump already uploaded."
+  exit;
+fi;
+g_remote=$(git remote add origin https://${{ secrets.GITLAB_TOKEN_NAME }}:${{ secrets.GITLAB_TOKEN }}@gitlab.com/${{ env.UN }}/lineage_device_${{ env.DB }}_${{ env.DCN }}.git)
+if [ ! -f "$g_remote" ]; then
   echo "Pushing failed!"
   echo "NOTE: Dump already uploaded."
   exit;
@@ -33,7 +40,7 @@ if [ ! -f "$g_commit" ]; then
   echo "NOTE: Dump already uploaded."
   exit;
 fi;
-g_upload=$(gh repo create lineage_device_${{ env.DB }}_${{ env.DCN }} --public --description="LineageOS Device tree for ${{ env.DCN }}." --source=. --remote=origin --push)
+g_upload=$(git push -u origin lineage-${{ env.DCN }})
 if [ ! -f "$g_upload" ]; then
   echo "Pushing failed!"
   echo "NOTE: Dump already uploaded."
