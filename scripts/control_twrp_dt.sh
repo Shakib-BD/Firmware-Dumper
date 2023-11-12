0842263cb12e5f120471a030a19516eb0b2c9c77
YZBruh <131263805+YZBruh@users.noreply.github.com>
2023-11-12 11:32:13 +0300

    They perform better

--- scripts/control_twrp_dt.sh
+++ scripts/control_twrp_dt.sh
@@ -9,16 +9,23 @@ if [ ! -f "$check_dt" ]; then
 fi;
 echo "A TWRP compatible device tree has been created."
 echo "Pushing..."
+sudo rm -rf .git
 g_init=$(git init)
 if [ ! -f "$g_init" ]; then
   echo "Pushing failed!"
-  echo "NOTE: Dump and LineageOS tree already uploaded."
+  echo "NOTE: Dump and LineageOS device tree already uploaded."
   exit;
 fi;
-g_branch=$(git branch -M twrp-${{ env.DCN }})
+g_branch=$(git branch -M android-${{ env.AV }})
 if [ ! -f "$g_branch" ]; then
   echo "Pushing failed!"
-  echo "NOTE: Dump and LineageOS tree already uploaded."
+  echo "NOTE: Dump and LineageOS device tree already uploaded."
+  exit;
+fi;
+g_remote=$(git remote add origin https://${{ secrets.GITLAB_TOKEN_NAME }}:${{ secrets.GITLAB_TOKEN }}@gitlab.com/${{ env.UN }}/twrp_device_${{ env.DB }}_${{ env.DCN }}.git)
+if [ ! -f "$g_remote" ]; then
+  echo "Pushing failed!"
+  echo "NOTE: Dump and LineageOS device tree already uploaded."
   exit;
 fi;
 g_add=$(git add .)
@@ -33,7 +40,7 @@ if [ ! -f "$g_commit" ]; then
   echo "NOTE: Dump and LineageOS tree already uploaded."
   exit;
 fi;
-g_upload=$(gh repo create twrp_device_${{ env.DB }}_${{ env.DCN }} --public --description="TWRP Device tree for ${{ env.DCN }}." --source=. --remote=origin --push)
+g_upload=$(git push -u origin android-${{ env.AV }})
 if [ ! -f "$g_upload" ]; then
   echo "Pushing failed!"
   echo "NOTE: Dump and LineageOS tree already uploaded."
